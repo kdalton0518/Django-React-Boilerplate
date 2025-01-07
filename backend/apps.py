21e13ec3c12ec70e4a743dc6d0ef2bef0ea87fc5
kdalton0518 <kdalton0518@gmail.com>
2025-01-07 03:29:33 -0500

    chore: update package versions

--- backend/apps.py
+++ backend/apps.py
@@ -4,6 +4,8 @@ from django.apps import AppConfig
 class BackendConfig(AppConfig):
     name = "backend"
     verbose_name = "Public website"
+    default_auto_field = 'django.db.models.BigAutoField'
+
 
     def ready(self):
         return
